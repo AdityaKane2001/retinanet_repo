5c7197c9f7426aaaed2691c45fd3f75f206ebac9
Adityakane2001 <adityakane1@gmail.com>
2021-03-03 08:49:14 +0000

    Version 21 Mar 03, 02:19 PM

--- train.py
+++ train.py
@@ -105,7 +105,7 @@ def main(args=None):
     optimizer = optim.Adam(retinanet.parameters(), lr=2e-5)
     #one_scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.1)
     lr_scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=3, verbose=True)
-    multistep_scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=[2,5,8,12], gamma=0.4)
+    multistep_scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=[1,5,8,12], gamma=0.5)
     loss_hist = collections.deque(maxlen=500)
     val_loss_hist = collections.deque(maxlen=500)
 
